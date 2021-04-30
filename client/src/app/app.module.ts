80b08220a43700212d87a0525bbafd5183fdcb50
StephenAvila2020 <stephen.r.avila@gmail.com>
2021-04-30 16:04:27 -0400

    Using Conditionals to show and remove content

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,14 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {HttpClientModule} from '@angular/common/http';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
