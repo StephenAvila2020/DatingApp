1e9e7db75eec6d176680b0346a68a4031e0fbaee
StephenAvila2020 <stephen.r.avila@gmail.com>
2021-05-03 16:38:08 -0400

    Completed register/home page/login

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,16 +4,24 @@ import {HttpClientModule} from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 import { FormsModule } from '@angular/forms';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavComponent
+    NavComponent,
+    HomeComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule,
+    BsDropdownModule.forRoot()
 
   ],
   providers: [],
